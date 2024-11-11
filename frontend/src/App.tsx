4a561983bcdd8349379b3219665e1f0eac02d013
ryan-crabbe <128659760+ryan-crabbe@users.noreply.github.com>
2024-11-10 16:51:23 -0800

    first

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen bg-background">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="flex justify-between items-center p-4 bg-background text-foreground shadow-sm">
+      <p className="text-xl font-bold text-primary">Blog Project</p>
+    </header>
+  );
+}
